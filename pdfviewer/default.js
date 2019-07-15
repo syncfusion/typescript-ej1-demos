7a600461ce90f9e040fa0dbd02ae1d99709545d5
root	esting <buildautomation@syncfusion.com>
2019-07-15 14:30:53 +0530

    v17.2.0.34

--- pdfviewer/default.js
+++
@@ -1,9 +0,0 @@
-var PDFViewerComponent;
-(function (PDFViewerComponent) {
-    $(function () {
-        var pdfviewerControl = new ej.PdfViewer($("#pdfviewer"), {
-            serviceUrl: window.baseurl + "api/PdfViewer",
-            isResponsive: true
-        });
-    });
-})(PDFViewerComponent || (PDFViewerComponent = {}));
