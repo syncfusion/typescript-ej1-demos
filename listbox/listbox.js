f115dca6805e103c85438598fe279186f3ae465b
SyncfusionInstall <buildautomation@syncfusion.com>
2020-07-07 15:19:33 +0530

    v18.2.0.44

--- listbox/listbox.js
+++
@@ -1,8 +0,0 @@
-var ListBoxComponent;
-(function (ListBoxComponent) {
-    $(function () {
-        var listboxInstance = new ej.ListBox($("#selectcar"), {
-            showCheckbox: true
-        });
-    });
-})(ListBoxComponent || (ListBoxComponent = {}));
