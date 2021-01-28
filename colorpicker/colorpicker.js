bdbf0426d776d2387c40cc96a27f93088f66f087
buildautomation <buildautomation@syncfusion.com>
2021-01-28 14:25:57 +0530

    v18.4.0.39

--- colorpicker/colorpicker.js
+++
@@ -1,8 +0,0 @@
-var ColorPickerComponent;
-(function (ColorPickerComponent) {
-    $(function () {
-        var colorSample = new ej.ColorPicker($("#colorpick"), {
-            value: "#278787"
-        });
-    });
-})(ColorPickerComponent || (ColorPickerComponent = {}));
