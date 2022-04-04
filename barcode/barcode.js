59d92df240b41dd5caa659816d3f2f5cd66e1345
SyncfusionBuild <buildautomation@syncfusion.com>
2022-04-04 15:27:27 +0530

    v20.1.0.47

--- barcode/barcode.js
+++
@@ -1,8 +0,0 @@
-var Barcodecomponent;
-(function (Barcodecomponent) {
-    $(function () {
-        var barcodesample = new ej.datavisualization.Barcode($("#Barcode"), {
-            text: "http://www.syncfusion.com"
-        });
-    });
-})(Barcodecomponent || (Barcodecomponent = {}));
