6d0d7c48f3fc9b79b57730ef3076556ce40a56ee
SyncfusionBuild <buildautomation@syncfusion.com>
2022-12-22 08:57:36 +0000

    v20.4.0.38

---
+++ colorpicker/colorpicker.ts
@@ -0,0 +1,10 @@
+﻿/// <reference path="../tsfiles/jquery.d.ts" />
+/// <reference path="../tsfiles/ej.web.all.d.ts" />
+
+module ColorPickerComponent {
+    $(function () {
+        var colorSample = new ej.ColorPicker($("#colorpick"), {
+            value: "#278787"
+        });
+    });
+}
